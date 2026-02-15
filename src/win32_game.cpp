8670803a2d66d46eb337dfe24d0b8cabdc428090
lee <lee.southern@protonmail.com>
2026-02-15 22:20:32 +0000

    More project setup

--- src/win32_game.cpp
+++ src/win32_game.cpp
@@ -3,11 +3,21 @@
 
 // WinMain is the entry point for C/C++ applications made on Windows
 int WINAPI WinMain(
-   HINSTANCE hInstance,
-   HINSTANCE hPrevInstance,
-   LPSTR     lpCmdLine,
-   int       nCmdShow
+  HINSTANCE hInstance,
+  HINSTANCE hPrevInstance,
+  LPSTR     lpCmdLine,
+  int       nCmdShow
 ) {
+
+  // GetModuleHandle(0);
+
+  MessageBox(
+    0,
+    (LPCTSTR)"Welcome to the Archive Engine",
+    (LPCTSTR)"The Archive Engine",
+    MB_OK | MB_ICONINFORMATION
+  );
+
   return 0;
 };
 
